294fde0aeeaaca5fe130833ffd456853f0c4e944
HARIOM-JHA01 <jhahariom040@gmail.com>
2023-06-30 16:59:29 +0530

    Add starting files and some starting js code

---
+++ game.js
@@ -0,0 +1,8 @@
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence(params) {
+  const randomNumber = Math.floor(Math.random() * 4);
+}
+
+let randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
